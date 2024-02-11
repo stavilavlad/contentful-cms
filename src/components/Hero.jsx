9976fcaaca8788fcf1555885c8711b3ef10ca199
Vlad <vladstavila@gmail.com>
2024-02-11 19:30:42 +0200

    added hero description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="hero-center">
         <div className="hero-title">
           <h1>Projects</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, ipsa accusamus. Fugit ipsa inventore unde nesciunt velit itaque minima labore quis sapiente voluptas mollitia at eaque, magni a vitae impedit?</p>
+          <p>This is a simple website to showcase my portofolio. Take a look!</p>
         </div>
         <div className="img-container">
           <img src={heroImg} alt="womand and browser" className="img" />
