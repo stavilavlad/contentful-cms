ddbc930cabbf2851eccb1783e5152efceec4b586
Vlad <vladstavila@gmail.com>
2024-03-06 12:53:06 +0200

    descript

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -7,7 +7,7 @@ const ProjectsCard = ({ url, img, github, title, description }) => {
       <img src={img} alt={title} className=" rounded-t-lg w-full object-cover h-64" />
       <div className="capitalize p-8">
         <h2 className="text-xl font-bold tracking-wide ">{title}</h2>
-        <p className="mt-4 text-slate-700 leading-relaxed">{description}</p>
+        <p className="mt-4 text-slate-700 leading-relaxed whitespace-pre-wrap">{description}</p>
         <div className="flex mt-4 gap-x-2">
           <a href={url} target="_blank">
             <TbWorldWww className="w-8 h-8 text-slate-600 hover:text-black" />
