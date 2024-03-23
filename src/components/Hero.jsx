1c33f1c9dc04bb790ef39d6545306548a4eb0efb
Vlad <vladstavila@gmail.com>
2024-03-23 14:50:47 +0200

    title change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="hero-center">
         <div className="hero-title">
           <h1>My Portfolio</h1>
-          <p>This is a simple website to showcase my portofolio. Take a look!</p>
+          <p>This is a simple website to showcase my portfolio. Take a look!</p>
         </div>
         <div className="img-container">
           <img src={heroImg} alt="womand and browser" className="img" />
